5a0315b04b86ff95a56ddba8bd5e77d823332b78
fraberret <f.berretta02@gmail.com>
2024-04-18 15:22:04 +0200

    create first object

--- index.php
+++ index.php
@@ -7,4 +7,13 @@ class Production{
 
 }
 
+$productionfirst = new Production();
+$productionfirst -> title='First Production';
+$productionfirst -> language='English';
+$productionfirst -> vote='1';
+
+
+var_dump($productionfirst)
+
+
 ?>
