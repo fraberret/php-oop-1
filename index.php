292f6dca0fa30189125188e635c54b346814a9de
fraberret <f.berretta02@gmail.com>
2024-04-18 16:02:59 +0200

    add object in page

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ class Production{
     public $language;
     public $vote;
 
+public function getTitle(){
+        return $this->title; 
+    }
+    public function getLanguage(){
+        return $this->language; 
+    }
+    public function getVote(){
+        return $this->vote; 
+    }
 
     public function __construct($title, $language, $vote) {
         $this->title = $title;
@@ -12,18 +21,24 @@ class Production{
         $this->vote = $vote;
     }
 
+    
+
 
 }
 
 $productionFirst = new Production('First Production', 'English', 1);
 
 
+echo $productionFirst->getTitle();
+echo $productionFirst->getLanguage();
+echo $productionFirst->getVote();
 
-var_dump($productionFirst);
 
 $productionSecond = new Production('Seconda Produzione', 'Italiano', 5);
 
+echo $productionSecond->getTitle();
+echo $productionSecond->getLanguage();
+echo $productionSecond->getVote();
 
-var_dump($productionSecond);
 
 ?>
