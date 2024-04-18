e929c562a70dfff3ccab95b750a324d0dc16fd7f
fraberret <f.berretta02@gmail.com>
2024-04-18 19:21:28 +0200

    create a folder ```db.php``` and create an array with the object with production class

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/Models/Productions.php';
+
+$productions =[
+    new Production("Film1", "Italiano", 8),
+    new Production("Serie TV1", "Inglese", 9)
+];
+
+?>
--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
 
-require_once __DIR__ . '/Models/Productions.php';
-
-$productionFirst = new Production("Harry Potter: L'Ordine Della Fenice", 'Italiano', 10);
-
-echo "Produzione 1:<br>";
-echo $productionFirst->getTitle()."<br>";
-echo $productionFirst->getLanguage()."<br>";
-echo $productionFirst->getVote()."<br> <br>";
-
-
-$productionSecond = new Production('Natale a Miami', 'Italiano', 5);
-
-echo "Produzione 2:<br>";
-echo $productionSecond->getTitle()."<br>";
-echo $productionSecond->getLanguage()."<br>";
-echo $productionSecond->getVote()."<br>";
-
 
 ?>
