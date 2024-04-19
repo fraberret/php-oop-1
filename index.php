a004cc60467df2dbaa6d35629e66e62f6cebcae1
fraberret <f.berretta02@gmail.com>
2024-04-19 10:10:50 +0200

    add boostrap

--- index.php
+++ index.php
@@ -10,21 +10,28 @@ require_once 'db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
 </head>
 <body>
+
+    <header>
+        <h1 class="text-center">Productions</h1>
+    </header>    
+
     <main>
+        <div class="container">
+            <ul class="list-group">
+                <?php foreach($productions as $production):?>
+                    <li class="list-group-item">>
+                        Title: <?php echo $production->getTitle();?><br>
+                        Language: <?php echo $production->getLanguage();?><br>
+                        Vote: <?php echo $production->getVote();?><br><br>
 
-    <h1>Productions</h1>
-    <ul>
-        <?php foreach($productions as $production):?>
-            <li>
-                Title:<?php echo $production->getTitle();?><br>
-                Language:<?php echo $production->getLanguage();?><br>
-                Vote:<?php echo $production->getVote();?><br><br>
-
-            </li>
-        <?php endforeach;?>
-    </ul>
+                    </li>
+                <?php endforeach;?>
+            </ul>
+        </div>
     </main>
 </body>
 </html>
